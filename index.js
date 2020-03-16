ab91e006de878be5a4f9744faee52e00f92bf141
bryanlim95 <60686136+bryanlim95@users.noreply.github.com>
2020-03-16 16:37:39 +0800

    Update index.js

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ var d1 = document.getElementById("img1");
 var d2 = document.getElementById("img2");
 var title = document.getElementById("title");
 
-d1.src = "//raw.githubusercontent.com/bryanlim95/dice/master/images/dice" + randomnumber1 + ".png";
+d1.src = "https://user-images.githubusercontent.com/60686136/76737723-46e60a80-67a4-11ea-9baa-9d2a9d05ef35.png";
 d2.src = "\\images\\dice" + randomnumber2 + ".png";
 if (randomnumber1 > randomnumber2) {
   title.innerHTML = "Player 1 Wins!";
