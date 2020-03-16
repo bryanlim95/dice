e44e047a23f3f8c290215d5c5e746304573b40a8
bryanlim95 <60686136+bryanlim95@users.noreply.github.com>
2020-03-16 15:27:52 +0800

    Add files via upload

---
+++ Dice/index.js
@@ -0,0 +1,15 @@
+var randomnumber1 = Math.floor(Math.random() * 6 + 1);
+var randomnumber2 = Math.floor(Math.random() * 6 + 1);
+var d1 = document.getElementById("img1");
+var d2 = document.getElementById("img2");
+var title = document.getElementById("title");
+
+d1.src = "D:\\Desktop\\Dicee Challenge - Starting Files\\images\\dice" + randomnumber1 + ".png";
+d2.src = "D:\\Desktop\\Dicee Challenge - Starting Files\\images\\dice" + randomnumber2 + ".png";
+if (randomnumber1 > randomnumber2) {
+  title.innerHTML = "Player 1 Wins!";
+} else if (randomnumber2 > randomnumber1) {
+  title.innerHTML = "Player 2 Wins!";
+} else {
+  title.innerHTML = "Draw!";
+}
