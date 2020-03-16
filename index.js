fbc899a2ad3c8534d85ba38e1f7391ec2366fea7
bryanlim95 <60686136+bryanlim95@users.noreply.github.com>
2020-03-16 16:49:05 +0800

    Update index.js

--- index.js
+++ index.js
@@ -4,8 +4,8 @@ var d1 = document.getElementById("img1");
 var d2 = document.getElementById("img2");
 var title = document.getElementById("title");
 
-d1.src = "master/images/dice" + randomnumber1 + ".png";
-d2.src = "master/images/dice" + randomnumber2 + ".png";
+d1.src = "images/dice" + randomnumber1 + ".png";
+d2.src = "images/dice" + randomnumber2 + ".png";
 if (randomnumber1 > randomnumber2) {
   title.innerHTML = "Player 1 Wins!";
 } else if (randomnumber2 > randomnumber1) {
